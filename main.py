ccbc8b593953964798b7c2cdaf23b16889d17f58
swish22 <166038301+swish22@users.noreply.github.com>
2025-07-05 21:34:24 -0400

    added automatic deployment logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 #handle redirects
 app = Flask(__name__)
@@ -25,6 +26,17 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
+# auto-update logic
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/swish22/mysite/seo_flask_demo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
   app.run(debug=True, host ="0.0.0.0")
