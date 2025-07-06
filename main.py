73c40afaf954cbc1ee0c6be7c057f17d3b3fbb53
Shane Williams <sw9990@princeton.edu>
2025-07-06 02:13:55 +0000

    added database stoarge

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 import git
+from flask_sqlalchemy import SQLAlchemy
 
 #handle redirects
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
 app.config['SECRET_KEY'] = '76195a107bcc4e72a1b43ac4cd5dca66'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+#create user class to store in database
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
 
 
 # webpage
@@ -17,20 +34,23 @@ app.config['SECRET_KEY'] = '76195a107bcc4e72a1b43ac4cd5dca66'
 def home():
     return render_template('home.html', title ='Home Page', subtitle='Welcome!', text='This is the home page')
 
+# about page
+@app.route("/about")
+def about():
+    return render_template('about.html', title ='About Page', subtitle='Learn about this site!', text='This is the about page')
 
 # form page
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        # get user fields and add to database
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
-
-# about page
-@app.route("/about")
-def about():
-    return render_template('about.html', title ='About Page', subtitle='Learn about this site!', text='This is the about page')
 
 # auto-update logic
 @app.route("/update_server", methods=['POST'])
